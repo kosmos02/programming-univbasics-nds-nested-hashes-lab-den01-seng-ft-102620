9cafefff3118d294671b5e2607ed34fb2a075330
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-05 19:18:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,7 @@ def literal_aoh
   roster = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
     
     ]
   
